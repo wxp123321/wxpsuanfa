03c384291d53455491152675c080c15a5b6ca993
wxp123321 <1648557071@qq.com>
2018-06-08 17:59:10 +0800

    双向链表

---
+++ doublyLinkedList.js
@@ -0,0 +1,85 @@
+function DoublyLinkedList(){
+	var Node = function(element){
+		this.element = element;
+		this.next = null;
+		this.prev = null;
+	}
+	var length = 0;
+	var head = null;
+	var tail = null;
+	
+	this.insert = function(position,element){
+		//检查越界值
+		if(position >= 0 && position <= length){
+			var node = new Node(element),
+				current = head,
+				previous,
+				index = 0;
+			if(position === 0){
+				if(!head){
+					head = node;
+					tail = node;
+				}else{
+					node.next = current;
+					current.prev = node;
+					head = node;
+				}
+			}else if(position === length){
+				current = tail;
+				current.next = node;
+				node.prev = current;
+				tail = node;
+			}else{
+				while(index++ < position){
+					previous = current;
+					current = current.next;
+				}
+				node.next = current;
+				previous.next = node;
+				
+				current.prev = node;
+				node.prev = previous;
+			}
+			length++;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	this.removeAt = function(position){
+		//检查越界值
+		if(position > -1 && position < length){
+			var current = head,
+				previous,
+				index = 0;
+		}
+		
+		//移除第一项
+		if(position === 0){
+			head = current.next;
+			
+			if(length === 1){
+				tail = null;
+			}else {
+				head,prev = null;
+			}
+		}else if(position === length-1){
+			current = tail;
+			tail = current.prev;
+			tail.next = null;
+		}else{
+			while(index++ < position){
+				previous = current;
+				current = current.next;
+			}
+			previous.next = current.next;
+			current.next.prev = previous;
+		}
+		length--;
+		return current.element;
+	}else {
+		return null;
+	}
+	
+}
