5b75eccf89b51aa713b6c41ad88c1b1617c7eca4
wxp123321 <1648557071@qq.com>
2018-06-09 16:43:29 +0800

    set集合

--- set.js
+++ set.js
@@ -31,6 +31,43 @@ function Set(){
 	this.values = function(){
 		return Object.keys(items);
 	}
+	//并集(将两个集合合并为一个)
+	this.union = function(otherSet){
+		var unionSet = new Set();
+		var values = this.values();
+		for(var i = 0;i<values.length;i++){
+			unionSet.add(values[i]);
+		}
+		values = otherSet.values();
+		for(var i = 0;i<values.length;i++){
+			unionSet.add(values[i]);
+		}
+		return unionSet;
+	}
+	
+	//交集(两个集合中相同元素)
+	this.intersection = function(otherSet){
+		var intersectionSet = new Set();
+		var values = this.values();
+		for(var i = 0;i<values.length;i++){
+			if(otherSet.has(values[i])){
+				intersectionSet.add(values[i]);
+			}
+		}
+		return intersectionSet;
+	}
+	
+	//差集(存在于集合A，但是不存在于集合B)
+	this.difference = function(otherSet){
+		var differenceSet = new Set();
+		
+		var values = this.values();
+		for(var i = 0;i<values.length;i++){
+			if(!otherSet.has(values[i])){
+				differenceSet.add(values[i]);
+			}
+		}
+	}
 }
 
 var set = new Set();
@@ -39,3 +76,23 @@ set.add(2);
 set.remove(1);
 console.log(set.values());
 console.log(set.size());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
